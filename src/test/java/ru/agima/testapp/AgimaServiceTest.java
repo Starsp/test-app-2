b54a5e0f17d19e525efcb7b676871e26a9776b1d
Васильев Георгий Дмитриевич <george.vassilev@softclub.by>
2024-03-26 14:49:03 +0300

    Initial commit

--- src/test/java/ru/agima/testapp/AgimaServiceTest.java
+++ src/test/java/ru/agima/testapp/AgimaServiceTest.java
@@ -1,8 +1,11 @@
 package ru.agima.testapp;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.agima.testapp.agima.domain.repository.AgimaRepository;
 import ru.agima.testapp.agima.service.AgimaService;
 
 import java.time.LocalDate;
@@ -12,10 +15,15 @@ class AgimaServiceTest {
 
     @Autowired
     private AgimaService agimaService;
+    @Autowired
+    private AgimaRepository agimaRepository;
+
 
     @Test
     void deleteTest() {
-        agimaService.delete(LocalDate.now());
+        agimaService.delete(LocalDate.now().minusDays(2));
+        Assertions.assertEquals(2L, agimaRepository.count());
+        Assertions.assertTrue(CollectionUtils.isNotEmpty(agimaRepository.findAll()));
     }
 
 }
